1d66d9d5a7e63a39bdd7849e94c90841cd815f1b
Marielle <marielle.ibias@example.com>
2022-08-17 16:20:07 +0200

    saving loading photos ok

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,4 +1,4 @@
-import { camera, trash, close } from "ionicons/icons";
+import { camera } from "ionicons/icons";
 import {
   IonContent,
   IonHeader,
@@ -12,9 +12,7 @@ import {
   IonRow,
   IonCol,
   IonImg,
-  IonActionSheet,
 } from "@ionic/react";
-import ExploreContainer from "../components/ExploreContainer";
 import { usePhotoGallery } from '../hooks/usePhotoGallery';
 import "./Tab2.css";
 
@@ -31,7 +29,7 @@ const Tab2: React.FC = () => {
       <IonContent>
   <IonGrid>
     <IonRow>
-      {photos.map((photo, index) => (
+      {photos.map((photo:any, index:any) => (
         <IonCol size="6" key={index}>
           <IonImg src={photo.webviewPath} />
         </IonCol>
