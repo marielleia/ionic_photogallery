e500ac276b8a5ede9a3d6462e6dbc891b52f1a2d
Marielle <marielle.ibias@example.com>
2022-08-17 13:59:17 +0200

    saving photos with camera updated

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -1,10 +1,9 @@
 import { useState, useEffect } from 'react';
-import { isPlatform } from '@ionic/react';
-
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
-import { Filesystem, Directory } from '@capacitor/filesystem';
+// import { isPlatform } from '@ionic/react';
+// import { Filesystem, Directory } from '@capacitor/filesystem';
 // import { Storage } from '@capacitor/storage';
-import { Capacitor } from '@capacitor/core';
+// import { Capacitor } from '@capacitor/core';
 
 export interface UserPhoto {
     filepath: string;
@@ -16,24 +15,47 @@ export function usePhotoGallery() {
 
     const takePhoto = async () => {
         const photo = await Camera.getPhoto({
-            resultType: CameraResultType.Uri,
-            source: CameraSource.Camera,
-            quality: 100,
+          resultType: CameraResultType.Uri,
+          source: CameraSource.Camera,
+          quality: 100,
         });
-
+      
         const fileName = new Date().getTime() + '.jpeg';
-    const newPhotos = [
-        {
+        const savedFileImage = await savePicture(photo, fileName);
+        const newPhotos = [savedFileImage, ...photos];
+        setPhotos(newPhotos);
+      };
+
+    const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
+        const base64Data = await base64FromPath(photo.webPath!);
+        const savedFile = await Filesystem.writeFile({
+            path: fileName,
+            data: base64Data,
+            directory: Directory.Data,
+        });
+        return {
+            photos,
+            takePhoto,
             filepath: fileName,
             webviewPath: photo.webPath,
-        },
-        ...photos,
-    ];
-    setPhotos(newPhotos);
-    };
+        };
 
-    return {
-        photos,
-        takePhoto,
     };
 }
+
+export async function base64FromPath(path: string): Promise<string> {
+    const response = await fetch(path);
+    const blob = await response.blob();
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        if (typeof reader.result === 'string') {
+          resolve(reader.result);
+        } else {
+          reject('method did not return a string');
+        }
+      };
+      reader.readAsDataURL(blob);
+    });
+  }
